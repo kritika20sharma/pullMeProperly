30b90db673a34f8bb1e5f32b1b2e6f9aa53000a8
kritika20sharma <53707301+kritika20sharma@users.noreply.github.com>
2019-10-29 22:37:14 +0530

    Update addFuctions.c

--- addFuctions.c
+++ addFuctions.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 
 int add(int a, int b);
+int sub(int a,int b);
 
-int main() {
+int main() 
+{
   int a = 10;
   int b = 20;
   int sum = add(a,b);
   printf("%d\n",sum);
+  int total = sub(a,b);
+  printf("%d\n",total);
 }
 
- int add(int a, int b) {
+ int add(int a, int b) 
+ {
   int result = a + b;
+  return result;
+}
+ int sub(int a, int b) 
+ {
+  int result = a - b;
   return result;
 }
